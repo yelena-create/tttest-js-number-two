af4eb3b7f98ab53dbbcd94a4c93a7dc3d518ee1c
Yakushko Yelena <82969672+yelena-create@users.noreply.github.com>
2022-01-24 18:55:41 +0200

    add pages

---
+++ js-pages/start-DOM.js
@@ -0,0 +1,4 @@
+const actionsEl = document.querySelectorAll('.js-actions button');
+      console.log(actionsEl);
+      console.log(actionsEl[0].dataset);
+      console.log(actionsEl[2].dataset.action);
